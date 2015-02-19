0016f81a9a9b7dcc1c589f2352ddca8d219cd27d
Charlie Guthrie <charlesdguthrie@gmail.com>
2015-02-18 23:36:11 -0500

    First commit for HW 3

--- hw2-lasso/ridge_skeleton.py
+++ hw2-lasso/ridge_skeleton.py
@@ -9,15 +9,15 @@ y = numpy.loadtxt("y.txt")
 w = numpy.random.rand(D,1)
 
 def ridge(Lambda):
-  def ridge_obj(theta):
-    return ((numpy.linalg.norm(numpy.dot(X,theta) - y))**2)/(2*N) + Lambda*(numpy.linalg.norm(theta))**2
-  return ridge_obj
+    def ridge_obj(theta):
+        return ((numpy.linalg.norm(numpy.dot(X,theta) - y))**2)/(2*N) + Lambda*(numpy.linalg.norm(theta))**2
+    return ridge_obj
 
 def compute_loss(Lambda, theta):
-  return ((numpy.linalg.norm(numpy.dot(X,theta) - y))**2)/(2*N)
+    return ((numpy.linalg.norm(numpy.dot(X,theta) - y))**2)/(2*N)
 
 for i in range(-5,6):
-  Lambda = 10**i;
-  w_opt = minimize(ridge(Lambda), w)
-  print Lambda, compute_loss(Lambda, w_opt.x)
+    Lambda = 10**i;
+    w_opt = minimize(ridge(Lambda), w)
+    print Lambda, compute_loss(Lambda, w_opt.x)
 
